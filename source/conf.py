5f9cc778777ade53ce5f464f68ee256ad2e77c88
RoddyMvp <roddymvp@gmail.com>
2019-01-14 10:41:47 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -39,7 +39,7 @@ from recommonmark.parser import CommonMarkParser
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
- #   'sphinx_markdown_tables',
+#   'sphinx_markdown_tables',
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
